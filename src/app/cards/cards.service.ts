52de685b7b20ff1203d34fc9963e708cad2b4c3d
anand-kashyap <anandkashyap60@gmail.com>
2019-07-27 21:55:05 +0530

    add clipboard

--- src/app/cards/cards.service.ts
+++ src/app/cards/cards.service.ts
@@ -86,6 +86,17 @@ export class CardsService {
     /* Clipboard.write({
       string: val
     }); */
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
     console.log('copied');
   }
 
